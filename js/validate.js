9e91e29932ee38df5fc1485840367b384df86789
Erik <Sabrina.Kynast@qplix.com>
2022-12-20 16:52:10 +0100

    WIp

--- js/validate.js
+++ js/validate.js
@@ -1,9 +1,3 @@
-//////CONTACT FORM VALIDATION
-var dorisText = "Lorem ipsum dolor sit amet, <br>consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo";
-
-var small = "Lorem ipsum dolor sit amet, <br>consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem";
-var encTest = "odh3Alp9pAyr4DnYOkjLqf9qrxlGCe4WsZvZJJDEW2EF4NE0769OkWse/lpK5rdax+5DqDF5HE5nvXuLxyQF/BlAyO/UxiuJ1H/nHXFsiiG8vp5OuZ/O7bYMjkn6Rqvpp1dxc4l2HhTkd+D0dmJeFqFJ230V6Vh2qZ1iISrDh1cmnhAHTuyFShe1PoEqvfEFPiVUpjMw/zMcu9FzBYfDp+IekKLwO+QddUi12aQlVb0b8YuwNbx5JdETfC1P8DpyFBNf9KBe0CaSGO+f/Y6ZIxAoW/i7xjWVlP2SL4JffTOFSdsny6+S2g5If8l1yeZ0UJov+k7XjNmohfghJ7vBTdIp1zTQQR6d+NkUkx1rOGV5KCG2E8a5J5/GAy0/irarY+xFs2SC1x3ZVDUMxGtFGkjl8J1aXCIeQhPqAr5IVAI70G2LqUB7TB3CBW66FPdeoWuqvwAutxVwR4rni7eg70FkwGo9mNagOIeg84De1AK7wHxVaUPBXz6UEoPVqNjy3OTnW1nOpp5FumwyXaLLWlw3KC1T0Xn7NHgQu6075l7on7xpbnTENrZbByEjr4N8nfsdXavxLMWHWDMPNrAz+nzoJRehDPmsaHHXr2VEXywFXR2eXm6D5JgWhLamXDuYGfr8eQIYXMV4L0Q5Yk9gL72TXJUdXmHa4E8EXKtBf2pvluGKe7jk3AYECDpTPNqtpYCNnKNLLkRvmn4l3DDfKuU9X7jwVKGx4HuBJi4EK9aNV0WkJfp9vhgm+CwO"
-
 var salt = "mySalt";
 var iv = "myIV";
 var parsedIV = CryptoJS.enc.Hex.parse(iv);
@@ -13,78 +7,56 @@ function capitalize(string) {
 }
 
 jQuery(document).ready(function ($) {
-
-	//if submit button is clicked
 	$('#submitPasswort').click(function () {
-
-		var password = $('#password').val();
+    var password = $('#password').val();
 		password = capitalize(password);
-    var passhash = CryptoJS.MD5(password).toString();
 
-		if (passhash === "9bf478b7174b37c4f7c4607cde146aaa") {
-			doris(password);
+    var key = CryptoJS.PBKDF2(
+              password,
+              CryptoJS.enc.Hex.parse(salt),
+              { keySize: this.keySize, iterations: this.iterationCount }
+          );
+  	var e = CryptoJS.Rabbit.encrypt(password, key, { iv: parsedIV });
+    var enc = e.ciphertext.toString(CryptoJS.enc.Base64);
+
+		if (enc === "vcVsCU04vg+x+w==") {
+			doris(password, true);
 			return;
-		} else if (passhash === "383c05bda6f030a44990d354b24f3338") {
-			kira(password);
+		} else if (enc === "ud5rHg==") {
+			kira(password, true);
 			return
-		} else if (passhash === "f97ca8decb3a5f04d11d0e3c6ff3f0cd") {
-			lea(password);
+		} else if (enc === "qcJmDA==") {
+			lea(password, true);
+			return
+		} else if (enc === "vt52E1Iv") {
+			anne(password, true);
 			return
 		}
-
-
-		alert(password);
-		alert(passhash);
 
 		//cancel the submit button default behaviours
 		return false;
 	});
 });
 
-/*
-Hinweise:
-1. Quack
-2.
-3. Dagobert ____
-*/
-function doris(password) {
-	setText(dorisText, password);
+
+function doris(password, decode) {
+  if (decode)
+	 setText(encDoris, password);
 }
 
-/*
-Hinweise:
-1. Quack
-2.
-3. Dagobert ____
-*/
-function lea(password) {
-	setEncText(small, password);
+function lea(password, decode) {
+  if (decode)
+	 setText(encLea, password);
 }
 
-/*
-Hinweise:
-1. DOTA
-2. Big
-3. Pink
-*/
-function kira(password) {
-	setText(encTest, password);
+function kira(password, decode) {
+  if (decode)
+	 setText(encKira, password);
 }
 
-function setEncText(text, password) {
-	var key = CryptoJS.PBKDF2(
-            password,
-            CryptoJS.enc.Hex.parse(salt),
-            { keySize: this.keySize, iterations: this.iterationCount }
-        );
-	var e = CryptoJS.Rabbit.encrypt(text, key, { iv: parsedIV });
-	console.log(e.cipherText);
-
-	jQuery(document).ready(function ($) {
-		$('#toBeFilled').html(text);
-
-		$('#encoded').html(e.ciphertext.toString(CryptoJS.enc.Base64));
-	});
+function anne(password, decode) {
+  if (decode)
+	 setText(encAnne, password);
 }
 
 function setText(text, password) {
@@ -104,8 +76,6 @@ function setText(text, password) {
   );
 
 	jQuery(document).ready(function ($) {
-		$('#toBeFilled').html(text);
-
 		$('#decoded').html(d.toString(CryptoJS.enc.Utf8));
 	});
 }
